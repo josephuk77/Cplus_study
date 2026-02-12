518575c30dcdb028b2d73b4914c36193b89f8007
josephuk77 <josephuk77@gmail.com>
2026-02-12 19:54:33 +0900

    study: add Character.cpp(ch02 data types)

---
+++ ch02_data_types/Character.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    char ch0 = 'a';
+    std::cout << "Character: " << ch0 << std::endl; // Character 출력 a
+
+    char ch1 = 97; // ASCII 코드 97은 'a'에 해당
+    std::cout << "Character from ASCII code 97: " << ch1 << std::endl; // Character from ASCII code 97 출력 a
+
+    return 0;
+
+}
